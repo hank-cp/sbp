86261cae51bed75788623a951e5bdab51bda6d23
Hank CP <true.cp@gmail.com>
2019-11-20 04:01:54 +0800

    update github actions

--- .github/actions/postgres/entrypoint.sh
+++ .github/actions/postgres/entrypoint.sh
@@ -8,24 +8,25 @@ docker_run="$docker_run -e POSTGRES_PASSWORD=$INPUT_POSTGRESQL_PASSWORD"
 if [ ! -z "$INPUT_POSTGRESQL_INIT_SCRIPTS" ]
 then
   PWD=`pwd`
+#  PWD="/Users/hank/Projects/Lolth/sbp"
   INIT_DB_DIR="$PWD/$INPUT_POSTGRESQL_INIT_SCRIPTS"
 
   [ ! -d "$INIT_DB_DIR" ] && echo "WARNING: directory $INIT_DB_DIR DOES NOT exist"
 
-  echo "$INIT_DB_DIR"
+  echo "workspace :: $GITHUB_WORKSPACE"
+  ls -l $GITHUB_WORKSPACE
+  echo "init_db_dir :: $INIT_DB_DIR"
   ls -l "$INIT_DB_DIR"
   docker_run="$docker_run -v $INIT_DB_DIR:/docker-entrypoint-initdb.d"
 fi
 
 docker_run="$docker_run --rm -p 5432:5432 postgres:$INPUT_POSTGRESQL_VERSION"
 
-echo "$INPUT_POSTGRESQL_CONF"
 if [ ! -z "$INPUT_POSTGRESQL_CONF" ]
 then
   docker_run="$docker_run -c '$INPUT_POSTGRESQL_CONF'"
 fi
 
-ls /docker-entrypoint-initdb.d
 echo "$docker_run"
 
 sh -c "$docker_run"
