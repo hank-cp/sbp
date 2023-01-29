0be59ffe53abf4c7ec05d15bec59a3178e36e82f
Hank CP <true.cp@gmail.com>
2023-01-30 02:59:55 +0800

    Separator flyway dependency

--- sbp-flyway-spring-boot-starter/src/main/java/org/laxture/sbp/spring/boot/FlywayClassLoaderConfiguration.java
+++ sbp-flyway-spring-boot-starter/src/main/java/org/laxture/sbp/spring/boot/FlywayClassLoaderConfiguration.java
@@ -25,7 +25,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
