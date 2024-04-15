383c2b8cf0ee4f21e27887f4325e3c5064092876
kenpb <kpalmab@icloud.com>
2024-04-15 03:12:01 +0000

    add thymeleaf plugin configurer

---
+++ sbp-spring-boot-starter-thymeleaf/src/main/java/org/laxture/sbp/spring/boot/SbpThymeleafConfigurer.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2019-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.laxture.sbp.spring.boot;
+
+import org.laxture.sbp.spring.boot.IPluginConfigurer;
+import org.laxture.sbp.spring.boot.SpringBootstrap;
+import org.springframework.context.support.GenericApplicationContext;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+
+/**
+ * @author <a href="https://github.com/kenpb">Kenneth P. Barquero</a>
+ */
+public class SbpThymeleafConfigurer implements IPluginConfigurer {
+
+    @Override
+    public String[] excludeConfigurations() {
+        return new String[] {
+                "org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration"
+        };
+    }
+
+    @Override
+    public void onBootstrap(SpringBootstrap bootstrap, GenericApplicationContext pluginApplicationContext) {
+        SpringTemplateEngine templateEngine = (SpringTemplateEngine) bootstrap.getMainApplicationContext()
+                .getBean("templateEngine");
+        templateEngine.addTemplateResolver(this.pluginTemplateResolver(pluginApplicationContext));
+    }
+
+    SpringResourceTemplateResolver pluginTemplateResolver(GenericApplicationContext pluginApplicationContext) {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext(pluginApplicationContext);
+        resolver.setPrefix("/templates/");
+        resolver.setSuffix(".html");
+        return resolver;
+    }
+
+}
